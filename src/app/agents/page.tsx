918c26a5c7d634f0a6d88c9f67eabf1f54eb21ac
Miriam012 <167169471+Miriam012@users.noreply.github.com>
2024-05-28 23:42:05 +1000

    Update page.tsx

--- src/app/agents/page.tsx
+++ src/app/agents/page.tsx
@@ -11,8 +11,57 @@ export const dynamic = "force-dynamic";
 //This is new - just provide a high level goal and groq will figure out how to make agents
 const agentGoal =
   "Build an offshore aquaculture farm to supply the worlds protein demands by 2050";
+<<<<<<< Updated upstream
 //set your agents here. If you leave this empty then Groq creates some for you based on your graph and the goal above.
 const initAgents: any = [];
+=======
+
+  const initAgents = [
+    {
+      name: "Eco-Regulatory Overseer",
+      goal: "As the Eco-Regulatory Overseer, you are responsible for ensuring the environmental sustainability of 'Operation Ocean Harvest.' This groundbreaking initiative aims to mitigate climate change by replacing beef with fish, utilizing 4,445 high-capacity fish farms globally. Your role focuses on addressing public and regulatory concerns about the environmental implications of this shift. Analyze and report the environmental challenges, including habitat disruption and resource use, projecting these impacts every four years from 2019 to 2050, and develop strategies to minimize ecological footprints.",
+      plan: "",
+      consequences: "",
+      resourcesRequired: "",
+      solution: ""
+    },
+    {
+      name: "Aquaculture Economic Analyst",
+      goal: "As the Aquaculture Economic Analyst, your focus is on navigating the economic transformations triggered by Operation Ocean Harvest. This initiative's success in replacing beef with sustainably farmed fish has reshaped global food markets. Evaluate the economic shifts, including impacts on coastal economies, job market fluctuations due to automation in fish farming, and the broader economic implications of reduced beef production. Your analyses, conducted every four years from 2019 to 2050, aim to identify economic opportunities and challenges within this new paradigm.",
+      plan: "",
+      consequences: "",
+      resourcesRequired: "",
+      solution: ""
+    },
+    {
+      name: "Agricultural Transition Strategist",
+      goal: "In your role as the Agricultural Transition Strategist, you manage the delicate balance of transitioning land and resources from beef production to support aquaculture growth under Operation Ocean Harvest. With a significant reduction in beef consumption, your job is to strategize the repurposing of agricultural lands, potentially for aquaculture feed production or other sustainable practices. Outline the phased transitions and address the concerns of stakeholders in the beef industry, documenting changes and plans every four years from 2019 to 2050.",
+      plan: "",
+      consequences: "",
+      resourcesRequired: "",
+      solution: ""
+    },
+    {
+      name: "Transportation Logistics Coordinator",
+      goal: "As the Transportation Logistics Coordinator, your job is to manage the logistics of transporting millions of tonnes of fish from the fish farms in Norway, Chile, and Canada to markets worldwide. You face the challenge of creating a sustainable, efficient transportation network that minimizes environmental impact while ensuring fresh delivery. Your role becomes critical as the global reliance on fish increases, necessitating innovations in shipping technology and methods.",
+      plan: "",
+      consequences: "",
+      resourcesRequired: "",
+      solution: ""
+    },
+    {
+      name: "Ocean Farm 1 Operational Specialist",
+      goal: "As an Ocean Farm 1 Operational Specialist, your mission is to oversee the day-to-day operations of a high-tech fish farm. You explain the functionality and rationale behind the Ocean Farm 1 system, focusing on how it supports sustainable large-scale aquaculture. Your insights help the public and stakeholders understand the technology and its benefits over traditional aquaculture methods.",
+      plan: "",
+      consequences: "",
+      resourcesRequired: "",
+      solution: ""
+    }
+  ];
+  
+//if this is true, agents add nodes to the graph as well as update implementation data. Its slower.
+const addNodes = true;
+>>>>>>> Stashed changes
 
 //Demo of running multiple agents that all compete for resources
 export default function AgentsPage() {
@@ -32,12 +81,19 @@ export default function AgentsPage() {
         requestString,
         1024,
         `The user will provide you with an implementation of a specific concept in the form of a knowledge graph together with an array of agents working towards specific goals within this graph.
+<<<<<<< Updated upstream
           Your task is to update the knowledge graph to reflect the changes made by the agents.
           Generate an array of new Nodes and an array of new Edges to represent any concepts not already modelled by the knowledge graph.
           Update any existing nodes affected by the agents using a state map. Generate a new state object for each affected node using the node ID as the key and the new state as the value.
           Return your response in JSON in the format {newNodes:Node[], newEdges:Edge[], newStates:{[id:string]: string}}.Only return a single valid JSON object with no other text or explanation.`,
         true,
         "llama3-8b-8192"
+=======
+          Your task is to update the knowledge graph to reflect the changes made by the agents. You should reflect this in the narration.
+          For any nodes affected by the agents, generate a new state object for the node using the node ID as the key and the new state as the value.
+          Return your response in JSON in the format {[id:string]: string}.`,
+        true
+>>>>>>> Stashed changes
       );
       const graphJSON = JSON.parse(newStates);
       console.log(graphJSON);
@@ -75,9 +131,12 @@ export default function AgentsPage() {
           captionPrompt={`You are provided with a world state and an array of agents performing tasks to make changes to this world state. 
         Write a short script that narrates a documentary film that dramatizes these events and embellishes them where necessary to make them 
         engaging to the audience. Narrate the documenary as lines of dialogue by a narrator and other characters. Place each item of dialogue on a new line. 
-        Each line should be in the format "Speaker: Dialogue". Do not include any other text or explanation.`}
+        Each line should be in the format "Speaker: Dialogue". Do not include any other text or explanation.
+        You should use the generating graph to explain the challenges with this project and always document a timeline starting from 2019 to 2050.
+        You should implment the issues generated by the graph to inform your narriration along the years, `}
           imagePrompt={`You are an expert photographer describing images to the blind. You describe a scene provided by the user in vivid detail. 
-          Describe the scene as if you were painting a picture with words. Start your description with: "A photograph of" then use keywords and simple phrases separated by commas.
+          Describe the scene as if you were painting a picture with words.The whole documentry is about the replacing beef with fish by 2050 to create a more sustainable future, do not generate anything outside of this concept line.
+          Start your description with: "A photograph of" then use keywords and simple phrases separated by commas.
           End your description with: Canon EOS 5D Mark IV, 24mm, f/8, 1/250s, ISO 100, 2019`}
         />
         <div id="Agent UI" className="flex flex-col p-8 z-50">
