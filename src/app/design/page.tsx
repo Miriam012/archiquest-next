21dd100916f943ed4db6bde5349dd5bc37360fc9
Miriam012 <167169471+Miriam012@users.noreply.github.com>
2024-06-11 16:42:21 +1000

    Update page.tsx

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -9,21 +9,61 @@ import Image from 'next/image';
 
 const images = [
   {
-    original: '/masterplan.jpg',
-    thumbnail: '/masterplan.jpg',
+    original: '/1.jpg',
+    thumbnail: '/1.jpg',
   },
   {
-    original: '/processing.jpg',
-    thumbnail: '/processing.jpg',
+    original: '/2.jpg',
+    thumbnail: '/2.jpg',
   },
   {
-    original: '/section.jpg',
-    thumbnail: '/section.jpg',
+    original: '/3.jpg',
+    thumbnail: '/3.jpg',
   },
   {
-    original: '/Zones.jpg',
-    thumbnail: '/Zones.jpg',
-  }
+    original: '/4.jpg',
+    thumbnail: '/4.jpg',
+  },
+  {
+    original: '/5.jpg',
+    thumbnail: '/5.jpg',
+  },
+  {
+    original: '/6.jpg',
+    thumbnail: '/6.jpg',
+  },
+  {
+    original: '/details.jpg',
+    thumbnail: '/details.jpg',
+  },
+  {
+    original: '/masterplanNew.jpg',
+    thumbnail: '/masterplanNew.jpg',
+  },
+  {
+    original: '/CALL OUTS 1.jpg',
+    thumbnail: '/CALL OUTS 1.jpg',
+  },
+  {
+    original: '/zones.jpg',
+    thumbnail: '/zones.jpg',
+  },
+  {
+    original: '/processingstuff.jpg',
+    thumbnail: '/processingstuff.jpg',
+  },
+  {
+    original: '/section 1.jpg',
+    thumbnail: '/section 1.jpg',
+  },
+  {
+    original: '/section long.jpg',
+    thumbnail: '/section long.jpg',
+  },
+  {
+    original: '/finalpanel.jpg',
+    thumbnail: '/finalpanel.jpg',
+  },
 ];
 
 export default function Design() {
@@ -44,7 +84,8 @@ export default function Design() {
     <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100">
       <h1 className="text-4xl font-bold mb-4 text-center">Design Gallery</h1>
       <p className="text-lg text-center mb-6 max-w-2xl">
-        In this gallery, you will find masterplans, sections, and plans of how the project will operate and run globally. You will also find some AI-generated images that reflect the project's design goals.
+        In this gallery you will find an array of drawings, there are masterplans of the distribution and growth of the FISH.FOR.FOOD initiative. There are sections of the Norway hub processing and distributing services, you will also find iterative images of the development of the FFF fish farms.
+        To view the images more closely just tap on the images!
       </p>
       <div className="w-full max-w-4xl">
         <ImageGallery
